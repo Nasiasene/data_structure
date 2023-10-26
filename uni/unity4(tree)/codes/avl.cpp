31108ff6307f47b8dc1ba28c98c4653651a258e8
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-10-25 22:36:12 -0300

    Create avl.cpp

---
+++ uni/unity4(tree)/codes/avl.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int value) {
+        key = value;
+        left = nullptr;
+        right = nullptr;
+        height = 1;
+    }
+};
+
+
+class AVLTree {
+    Node* root;
+
+    int getHeight(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return node->height;
+    }
+
+    int getBalanceFactor(Node* node) {
+        if (node == nullptr)
+            return 0;
+        return getHeight(node->left) - getHeight(node->right);
+    }
+
+    Node* rotateRight(Node* y) {
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+        x->right = y;
+        y->left = T2;
+
+        y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+        x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* rotateLeft(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+        y->left = x;
+        x->right = T2;
+
+        x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+        y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+        return y;
+    }
+
+    Node* insert(Node* node, int key) {
+        if (node == nullptr)
+            return new Node(key);
+
+        if (key < node->key)
+            node->left = insert(node->left, key);
+        else if (key > node->key)
+            node->right = insert(node->right, key);
+        else
+            return node;
+
+        node->height = 1 + max(getHeight(node->left), getHeight(node->right));
+
+        int balance = getBalanceFactor(node);
+
+        if (balance > 1 && key < node->left->key)
+            return rotateRight(node);
+
+        if (balance < -1 && key > node->right->key)
+            return rotateLeft(node);
+
+        if (balance > 1 && key > node->left->key) {
+            node->left = rotateLeft(node->left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && key < node->right->key) {
+            node->right = rotateRight(node->right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+public:
+    AVLTree() {
+        root = nullptr;
+    }
+
+    void insert(int key) {
+        root = insert(root, key);
+    }
+
+    void inorderTraversal(Node* node) {
+        if (node != nullptr) {
+            inorderTraversal(node->left);
+            cout << node->key << " ";
+            inorderTraversal(node->right);
+        }
+    }
+
+    void inorder() {
+        inorderTraversal(root);
+        cout << endl;
+    }
+};
+
+int main() {
+    AVLTree avl;
+    avl.insert(10);
+    avl.insert(20);
+    avl.insert(30);
+    avl.insert(40);
+    avl.insert(50);
+    avl.insert(25);
+
+    cout << "Inorder Traversal of AVL Tree: ";
+    avl.inorder();
+
+    return 0;
+}
