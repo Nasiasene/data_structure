5bf27ad8a4f8a3afa957e254b565ac79110e611e
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2024-03-05 12:48:02 -0300

    binary search tree

---
+++ uni/DS2/unity1(main_memory_search)/1.3_hierarchical_search/1.3.1_binary_search_tree/code/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Função para inserir um novo nó na árvore
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+// Função para percorrer a árvore em ordem (inorder)
+void inorderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    
+    // Criando a árvore binária
+    root = createNode(50);
+    root->left = createNode(30);
+    root->right = createNode(70);
+    root->left->left = createNode(20);
+    root->left->right = createNode(40);
+    root->right->left = createNode(60);
+    root->right->right = createNode(80);
+
+    printf("Árvore em ordem (inorder): ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
